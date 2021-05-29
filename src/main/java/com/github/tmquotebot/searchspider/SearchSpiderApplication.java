fe1a959e901ba8dd5300fbf866d6733deb264deb
Arseny Druzhinin <senyasdr@yandex-team.ru>
2021-05-29 12:04:00 +0300

    Init commit

---
+++ src/main/java/com/github/tmquotebot/searchspider/SearchSpiderApplication.java
@@ -0,0 +1,13 @@
+package com.github.tmquotebot.searchspider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchSpiderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchSpiderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/tmquotebot/searchspider/SearchSpiderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tmquotebot.searchspider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchSpiderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
